a86a2b08b0a539a008c10ef829d213eea5b44076
ezeevisionchampua <boardmentorcbse@gmail.com>
2026-08-04 15:09:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// Sticky Navbar
 
+window.addEventListener("scroll",()=>{
+
+const nav=document.querySelector("nav");
+
+nav.classList.toggle("scrolled",window.scrollY>40);
+
+});
+
+// Mobile Menu
+
+const menu=document.querySelector(".menu");
+
+const navLinks=document.querySelector("nav ul");
+
+menu.onclick=()=>{
+
+navLinks.classList.toggle("active");
+
+};
